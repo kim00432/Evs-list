2e542b769b3bff1c00cc9b84577259b0c5d31965
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2021-11-17 18:05:24 -0500

    update paths for saved images for each data object

--- datasource/data.js
+++ datasource/data.js
@@ -5,70 +5,70 @@ const data = [
     make: 'Tesla',
     model: 'Model X',
     price: 129990,
-    img: '/#'
+    img: '/public/img/TeslaModelX.jpeg'
   },
   {
     id: 'abcd0003',
     make: 'BMW',
     model: 'i3s',
     price: 47650,
-    img: '/#'
+    img: '/public/img/BMWi3S/jpeg'
   },
   {
     id: 'abcd0006',
     make: 'Volvo',
     model: 'C40 Recharge',
     price: 58750,
-    img: '/#'
+    img: '/public/img/VolvoC40Recharge.jpeg'
   },
   {
     id: 'abcd0009',
     make: 'Ford',
     model: 'Mustang Mach E',
     price: 42895,
-    img: '/#'
+    img: '/public/img/MustangMachE.jpeg'
   },
   {
     id: 'abcd0012',
     make: 'GMC',
     model: 'Hummer E1',
     price: 112595,
-    img: '/#'
+    img: '/public/img/GMCHummerE1.jpeg'
   },
   {
     id: 'abcd0015',
     make: 'Lucid',
     model: 'Air',
     price: 169000,
-    img: '/#'
+    img: '/public/img/LucidAir.jpeg'
   },
   {
     id: 'abcd0018',
     make: 'Nissan',
     model: 'Leaf S',
     price: 27400,
-    img: '/#'
+    img: '/public/img/NissanLeafS.jpeg'
   },
   {
     id: 'abcd0021',
     make: 'Polestar',
     model: '2',
     price: 45900,
-    img: '/#'
+    img: '/public/img/Polestar2.jpeg'
   },
   {
     id: 'abcd0024',
     make: 'Porsche',
     model: 'Taycan',
     price: 82700,
-    img: '/#'
+    img: '/public/img/PorscheTaycan.jpeg'
   },
   {
     id: 'abcd0027',
     make: 'Tesla',
     model: 'Model 3',
     price: 44990,
-    img: '/#'
+    img: '/public/img/TeslaModel3'
   }
 ]
 
