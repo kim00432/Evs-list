e7a12fa9723ba9fa8f063d0108ad65624c47cc78
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2021-11-22 20:55:08 -0500

    remove starter code styling

--- pages/index.js
+++ pages/index.js
@@ -4,24 +4,13 @@ import Link from 'next/link'
 // import styles from '../styles/Index.module.css';
 
 export default function Home () {
-  // console.log(notes); //props.notes
-
   return (
-    <div sx={{ height: `calc(100vh - 60px)` }}>
-      <div
-        sx={{
-          variant: 'containers.page',
-          display: 'flex',
-          alignItems: 'flex-start',
-          height: '100%'
-        }}
-      >
+    <div>
+      <div>
         <div>
-          <h1 sx={{ fontSize: 8, p: 2 }}>Cars App</h1>
-          <p sx={{ p: 2 }}>
-            Click the Cars link at the top to see all our cars.
-          </p>
-          <p sx={{ p: 2 }}>
+          <h1>Cars App</h1>
+          <p>Click the Cars link at the top to see all our cars.</p>
+          <p>
             <Link href='/cars'>
               <a>Or tap here if this is closer</a>
             </Link>
