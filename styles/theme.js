f7d2a53c8472275c0b1e761e9501e8ae28a9606f
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2021-11-22 20:54:50 -0500

    remove polaris default theme from starter code. will create custom theme

--- styles/theme.js
+++ styles/theme.js
@@ -1,18 +1,10 @@
-import { polaris } from '@theme-ui/presets';
-
-//see https://theme-ui.com/demo for Polaris colors
-//see /styles/polaris-ref.json for full Polaris theme
-//config - https://theme-ui.com/theming#configuration-flags
-
 const theme = {
-  ...polaris,
   config: {
-    initialColorModeName: 'bubba',
+    initialColorModeName: 'JC-Yoona',
     useBorderBox: true,
-    useLocalStorage: true, //to save mode in localstorage
+    useLocalStorage: true //to save mode in localstorage
   },
   colors: {
-    karim: '#bada55', //we can make up color names
     text: '#454f5b',
     background: '#fff',
     primary: '#5c6ac4',
@@ -29,28 +21,25 @@ const theme = {
         primary: '#9c6ade',
         secondary: '#b4e1fa',
         highlight: '#b7ecec',
-        muted: '#e6e6e6',
-      },
-    },
+        muted: '#e6e6e6'
+      }
+    }
   },
   containers: {
     page: {
       width: '100%',
       maxWidth: '960px',
       m: 0,
-      mx: 'auto',
+      mx: 'auto'
     },
     card: {
       boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
       border: '1px solid',
       borderColor: 'muted',
       borderRadius: '4px',
-      p: 2,
-    },
-  },
-  styles: {
-    ...polaris.styles,
-  },
-};
+      p: 2
+    }
+  }
+}
 
-export default theme;
+export default theme
