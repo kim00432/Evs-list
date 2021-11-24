74867dcf895f1abea900bfff5d1c8db348c4a302
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2021-11-24 10:44:06 -0500

    update homepage styling

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,31 @@
 
 import Link from 'next/link'
 import styles from '../styles/Index.module.css'
+import theme from '../styles/theme'
 
 export default function Home () {
   return (
-    <div>
+    <div sx={{ ...theme.containers.page }}>
       <div>
         <div>
-          <h1>Cars App</h1>
-          <p>Click the Cars link at the top to see all our cars.</p>
+          <h1 sx={theme.fontSizes.subHeader}>
+            Welcome to our electronic vehicles database
+          </h1>
+          <p>
+            Here you will be able to compare prices across different types of
+            cars. Tap the link to get started!
+          </p>
           <p>
             <Link href='/cars'>
-              <a>Or tap here if this is closer</a>
+              <a
+                sx={{
+                  ...theme.fontSizes.headerLink,
+                  ...theme.colors.darkenAccent
+                }}
+              >
+                Go to car list
+              </a>
             </Link>
-            .
           </p>
         </div>
       </div>
