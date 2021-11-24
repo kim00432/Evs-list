9d251cf864bec42583004e1cb5a4062c5ab6d9ed
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2021-11-24 10:32:33 -0500

    hard code height for footer

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,11 @@ import theme from '../styles/theme'
 export default function Footer () {
   return (
     <footer
-      sx={{ ...theme.containers.fullWidthContainer, justifyContent: 'center' }}
+      sx={{
+        ...theme.containers.fullWidthContainer,
+        justifyContent: 'center',
+        minHeight: '10vh'
+      }}
     >
       <p sx={{ ...theme.fontSizes.callout, ...theme.colors.lightBody }}>
         &copy; 2021 Yoona Kim and JC Castagne
