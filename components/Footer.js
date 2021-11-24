b74fa8781671dfb7ea096583c4ff4362ce557701
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2021-11-24 10:10:12 -0500

    complete footer styling

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,15 @@
 /** @jsxImportSource theme-ui */
 
+import theme from '../styles/theme'
+
 export default function Footer () {
   return (
-    <footer>
-      <p>&copy; 2021 Yoona Kim and JC Castagne</p>
+    <footer
+      sx={{ ...theme.containers.fullWidthContainer, justifyContent: 'center' }}
+    >
+      <p sx={{ ...theme.fontSizes.callout, ...theme.colors.lightBody }}>
+        &copy; 2021 Yoona Kim and JC Castagne
+      </p>
     </footer>
   )
 }
